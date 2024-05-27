e1557df2e31a4f873617f48332f8924089c69a9d
Justine Gauthier <gauthier.justine25000@gmail.com>
2024-05-27 16:51:43 +0200

    Add update route and delete route

--- routes/offer.js
+++ routes/offer.js
@@ -21,9 +21,12 @@ router.post("/publish", isAuthenticated, fileupload(), async (req, res) => {
             color
         } = req.body;
 
-        const pictureData = await cloudinary.uploader.upload(
-            convertToBase64(req.files.picture)
-        );
+        let pictureData;
+        if (req.files && req.files.picture) {
+            pictureData = await cloudinary.uploader.upload(
+                convertToBase64(req.files.picture)
+            );
+        }
 
         const offer = new Offer({
             product_name: title,
@@ -96,5 +99,87 @@ router.get("/", async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
+
+router.put("/update/:id", isAuthenticated, fileupload(), async (req, res) => {
+    try {
+        const offer_id = req.params.id;
+        if (offer_id) {
+            const {
+                title,
+                description,
+                price,
+                condition,
+                city,
+                brand,
+                size,
+                color
+            } = req.body;
+
+            // Vérifiez que req.user est défini
+            if (!req.user) {
+                return res.status(401).json({ message: "Utilisateur non authentifié" });
+            }
+
+            // Récupérer l'annonce à mettre à jour
+            const offer = await Offer.findById(offer_id);
+            if (!offer) {
+                return res.status(404).json({ message: "Annonce non trouvée" });
+            }
+
+            // Vérifiez que offer.user est défini et correspond à l'utilisateur authentifié
+            if (!offer.owner._id || offer.owner._id.toString() !== req.user._id.toString()) {
+                return res.status(403).json({ message: "Accès refusé, vous n'êtes pas le propriétaire de cette annonce" });
+            }
+
+            let pictureData;
+            if (req.files && req.files.picture) {
+                pictureData = await cloudinary.uploader.upload(
+                    convertToBase64(req.files.picture)
+                );
+            }
+
+            const updatedOffer = await Offer.findOneAndUpdate(
+                { _id: offer_id },
+                {
+                    product_name: title,
+                    product_description: description,
+                    product_price: price,
+                    product_details: [
+                        { MARQUE: brand },
+                        { TAILLE: size },
+                        { ETAT: condition },
+                        { COULEUR: color },
+                        { EMPLACEMENT: city }
+                    ],
+                    ...(pictureData && { product_image: pictureData })
+                },
+                { new: true } // Retourne le document mis à jour
+            );
+
+            res.json(updatedOffer);
+        } else {
+            res.status(400).json({ messsage: "Id de l'annonce manquant !" });
+        }
+
+    } catch (error) {
+        console.log("error", error);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.delete("/delete/:id", isAuthenticated, fileupload(), async (req, res) => {
+    try {
+        if (req.params.id) {
+            await Offer.findByIdAndDelete(req.params.id);
+
+            res.json({ message: "Offer removed" });
+        } else {
+            res.status(400).json({ messsage: "Id de l'annonce manquant !" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 
 module.exports = router;
